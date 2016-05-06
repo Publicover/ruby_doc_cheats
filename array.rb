16256320b2ab6f5ab38bb0911d547f43dc09f0ec
Publicover <jameslpublicover@gmail.com>
2016-05-06 17:28:54 -0400

    access elements

--- array.rb
+++ array.rb
@@ -109,6 +109,105 @@ new_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 new_array.drop(5)
   => [5, 6, 7, 8, 9]
 
+----------
+
+GETTING INFORMATION
+
+length:
+to get the number of elements in an array, use .length, .count or .size
+
+new_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+new_array.length
+  => 10
+new_array.count
+  => 10
+new_array.size
+  => 10
+
+empty method:
+see if array contains anything at all with .empty?
+
+new_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+new_array.empty?
+  => false
+
+include method:
+see if array contains element with .include?
+
+new_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+new_array.include?(5)
+  => true
+
+----------
+
+ADD ELEMENTS
+
+.push(obj) and shovel method:
+add element to end of array with .push(obj) or <<
+
+new_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+new_array.push("farts")
+  => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "new_thing"]
+new_array << "new_thing"
+  => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "new_thing"]
+
+unshift(obj) method:
+add element to beginning of an array
+
+new_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+new_array.unshift("new_thing")
+  => ["new_thing", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+insert(pos, obj) method:
+use two arguments to add element to any position
+use more than two to add more than one element to any position
+
+new_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+new_array.insert(5, "new_thing")
+  => [0, 1, 2, 3, 4, "new_thing", 5, 6, 7, 8, 9]
+new_array.insert(5, "ohman", "newstuff", "righthere")
+  => [0, 1, 2, 3, 4, "ohman", "newstuff", "righthere", 5, 6, 7, 8, 9]
+
+----------
+
+REMOVE ELEMENT
+
+.pop method:
+remove and return last element
+
+new_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+new_array.pop
+  => 9
+
+.shift method:
+remove an return first element
+
+new_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+new_array.shift
+  => 0
+
+.delete_at(n) method:
+delete element at index
+
+new_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+new_array.delete_at(3)
+  => 3
+  new_array => [0, 1, 2, 4, 5, 6, 7, 8, 9]
+
+.delete method:
+delete element by name at any index
+
+new_array = ["a", "b", "c"]
+new_array.delete("b")
+  => "b"
+  new_array => ["a", "c"]
+
+.compact method:
+remove all nil values from array
+
+new_array = [nil, nil, nil, "freddy"]
+new_array.compact
+  => ["freddy"]
 
 
 
@@ -121,4 +220,16 @@ new_array.drop(5)
 
 
 
-  #
+
+
+
+
+
+
+
+
+
+
+
+
+#
