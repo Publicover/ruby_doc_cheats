cc00806d1a9d17676432e02d11cef7ede55cc583
Publicover <jameslpublicover@gmail.com>
2016-05-06 18:08:39 -0400

    array definition and class methods

--- array.rb
+++ array.rb
@@ -209,6 +209,104 @@ new_array = [nil, nil, nil, "freddy"]
 new_array.compact
   => ["freddy"]
 
+.uniq method:
+.uniq returns array of unique elements
+.uniq! is destructive
+
+new_array = ["jim", "jim", "publicover", 3, "jim", "dillehunt"]
+new_array.uniq
+  => ["jim", "publicover", 3, "dillehunt"]
+  new_array => ["jim", "jim", "publicover", 3, "jim", "dillehunt"]
+new_array.uniq!
+  => ["jim", "publicover", 3, "dillehunt"]
+  new_array => ["jim", "publicover", 3, "dillehunt"]
+
+----------
+
+ITERATE OVER ELEMENT
+
+.each method:
+iterates each element through the block. leaving original array intact
+
+new_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+new_array.each {|element| print element + 20}
+  20212223242526272829=> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+new_array.each do |element|
+  print element + 20
+end
+  20212223242526272829=> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+.reverse_each method:
+iterates each element throught the block in reverse order, leaving original array intact
+
+new_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+new_array.reverse_each {|element| print element + 20}
+  29282726252423222120=> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+.map method:
+modifies array based on passing each element through the block
+
+new_array = [1, 2, 3, 4, 5]
+new_array.map {|element| element + 20}
+  => [21, 22, 23, 24, 25]
+new_array.map {|element| element.to_s + "wutwut"}
+  => ["1wutwut", "2wutwut", "3wutwut", "4wutwut", "5wutwut"]
+  new_array => [1, 2, 3, 4, 5]
+
+----------
+
+SELECT ELEMENTS
+
+.select method:
+.select returns values defined in block
+.select! destroys original array and returns values defined in block
+
+new_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+new_array.select {|element| element < 6}
+  => [0, 1, 2, 3, 4, 5]
+  new_array => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+new_array.select! {|element| element < 6}
+  => [0, 1, 2, 3, 4, 5]
+new_array => [0, 1, 2, 3, 4, 5]
+
+.reject method:
+.reject returns elements that are false in the block
+.reject! destroys original array and returns elements that are false in the block
+
+new_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+new_array.reject {|element| element < 6}
+  => [6, 7, 8, 9]
+  new_array => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+new_array.reject! {|element| element < 6}
+  => [6, 7, 8, 9]
+  new_array => [6, 7, 8, 9]
+
+.drop_while method:
+returns elements that are true in the block
+
+new_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+new_array.drop_while {|element| element < 6}
+  => [6, 7, 8, 9]
+  new_array => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+.delete_if method:
+destroys original array and returns elements false in the block
+
+new_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+new_array.delete_if {|element| element < 6}
+  => [6, 7, 8, 9]
+  new_array => [6, 7, 8, 9]
+
+keep_if method:
+destorys original array and returns elements true in the block
+
+new_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+new_array.keep_if {|element| element < 6}
+  => [0, 1, 2, 3, 4, 5]
+  new_array => [0, 1, 2, 3, 4, 5]
+
+
+
 
 
 
